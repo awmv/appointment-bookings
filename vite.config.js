78bda9a10c361bf5beda7d34cbdf26b2f31ff445
Robert Richter <robert.richter@innotix.com>
2024-06-08 19:47:38 +0200

    feat: [AB-0002] Typescript support

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: true,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
+  meteor: {
+    clientEntry: "imports/ui/main.ts",
+  },
+  optimizeDeps: {
+    exclude: ["vue-meteor-tracker"],
+  },
+});
